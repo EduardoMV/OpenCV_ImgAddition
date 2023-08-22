8e25b1848f8f9a7616ef28fa4a4ad18225ba059e
Eduardo Ulises Martínez Vaca <81331013+EduardoMV@users.noreply.github.com>
2023-08-22 15:24:30 -0600

    ColorMix

--- main.cpp
+++ main.cpp
@@ -27,6 +27,56 @@ void GrayMix(){
     waitKey(0);
 }
 
+void ColorMix(){
+    Mat f = imread("../flower.jpg");
+    Mat g = imread("../Dog.jpg");
+
+    int srows = min(f.rows, g.rows);
+    int scols = min(f.cols, g.cols);
+
+    Mat h(srows, scols, CV_8UC3, Scalar(0, 0, 0));
+
+    for(int row = 0; row < srows; row++){
+        for(int col = 0; col < scols; col++){
+            for (int channel = 0; channel < 3; channel++) {
+                h.at<Vec3b>(row,col) = (Vec3b)(((f.at<Vec3b>(row,col) + g.at<Vec3b>(row,col))/2));
+            }
+        }
+    }
+
+    namedWindow("PhotoFrame", WINDOW_NORMAL);
+    imshow("PhotoFrame", h);
+    imwrite("ColorMix.jpg", h);
+    waitKey(0);
+}
+
+void constantAdd(){
+    Mat f = imread("../flower.jpg", IMREAD_GRAYSCALE);
+
+    Mat h(f.rows, f.cols, CV_8U, Scalar(0));
+
+    for(int row = 0; row < f.rows; row++){
+        for(int col = 0; col < f.cols; col++){
+            int color = f.at<uchar>(row, col) + 50;
+
+            if(color > 255){
+                h.at<uchar>(row,col) = (uchar)(255);
+            }
+            else{
+                h.at<uchar>(row,col) = (uchar)(color);
+            }
+
+        }
+    }
+
+    namedWindow("PhotoFrame", WINDOW_NORMAL);
+    imshow("PhotoFrame", h);
+    imwrite("Addition.jpg", h);
+    waitKey(0);
+    imshow("PhotoFrame2", f);
+    waitKey(0);
+}
+
 int main() {
-    GrayMix();
+    constantAdd();
 }
